6bdeebb905c1965139b764b82df597f5c2cf0180
Antarctica000 <1162953505@qq.com>
2020-01-20 20:09:23 +0800

    MyBatis代码生成器

---
+++ mybatis4/src/test/java/com/example/mybatis4/Mybatis4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mybatis4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mybatis4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
