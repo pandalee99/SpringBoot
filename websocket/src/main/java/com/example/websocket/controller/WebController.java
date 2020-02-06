b41c17eb8b5938df2751a54a4a862bb559668149
antarctica000 <1162053505@qq.com>
2020-02-06 16:18:12 +0800

    WebSocket的练习
    打造一个简易的聊天室

---
+++ websocket/src/main/java/com/example/websocket/controller/WebController.java
@@ -0,0 +1,36 @@
+package com.example.websocket.controller;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@RestController
+public class WebController {
+    /**
+     * 登陆界面
+     */
+    @GetMapping("/")
+    public ModelAndView login() {
+        return new ModelAndView("/login");
+    }
+
+    /**
+     * 聊天界面
+     */
+    @GetMapping("/index")
+    public ModelAndView index(String username, String password, HttpServletRequest request) throws UnknownHostException {
+        if (StringUtils.isEmpty(username)) {
+            username = "匿名用户";
+        }
+        ModelAndView mav = new ModelAndView("/chat");
+        mav.addObject("username", username);
+        mav.addObject("webSocketUrl", "ws://"+ InetAddress.getLocalHost().getHostAddress()+":"+request.getServerPort()+request.getContextPath()+"/chat");
+        return mav;
+    }
+
+}
